eee2ef9ca09770c2fb9e815caddd3d84cd635a0f
David <9419610+mutex-lock@users.noreply.github.com>
2025-02-03 18:42:47 -0500

    Logging macros respect config
    - `$print`, `$warn`, and `$error` updated to respect the configuration's `enabled` property.
    - Fixes roblox-aurora/rbxts-transform-debug#16

--- src/transform/macros/call/logging.ts
+++ src/transform/macros/call/logging.ts
@@ -8,10 +8,14 @@ export const PrintMacro: CallMacro = {
 		return state.symbolProvider.moduleFile!.get("$print");
 	},
 	transform(state: TransformState, node: ts.CallExpression) {
-		return factory.updateCallExpression(node, factory.createIdentifier("print"), undefined, [
-			createDebugPrefixLiteral(node),
-			...node.arguments,
-		]);
+		const { enabled } = state.config;
+
+		return enabled
+			? factory.updateCallExpression(node, factory.createIdentifier("print"), undefined, [
+					createDebugPrefixLiteral(node),
+					...node.arguments,
+				])
+			: factory.createVoidExpression(factory.createIdentifier("undefined"));
 	},
 };
 
@@ -20,10 +24,14 @@ export const WarnMacro: CallMacro = {
 		return state.symbolProvider.moduleFile!.get("$warn");
 	},
 	transform(state: TransformState, node: ts.CallExpression) {
-		return factory.updateCallExpression(node, factory.createIdentifier("warn"), undefined, [
-			createDebugPrefixLiteral(node),
-			...node.arguments,
-		]);
+		const { enabled } = state.config;
+
+		return enabled
+			? factory.updateCallExpression(node, factory.createIdentifier("warn"), undefined, [
+					createDebugPrefixLiteral(node),
+					...node.arguments,
+				])
+			: factory.createVoidExpression(factory.createIdentifier("undefined"));
 	},
 };
 
@@ -32,9 +40,13 @@ export const ErrorMacro: CallMacro = {
 		return state.symbolProvider.moduleFile!.get("$error");
 	},
 	transform(state: TransformState, node: ts.CallExpression) {
-		return factory.updateCallExpression(node, factory.createIdentifier("error"), undefined, [
-			createErrorPrefixLiteral(node),
-			...node.arguments.slice(1),
-		]);
+		const { enabled } = state.config;
+
+		return enabled
+			? factory.updateCallExpression(node, factory.createIdentifier("error"), undefined, [
+					createErrorPrefixLiteral(node),
+					...node.arguments.slice(1),
+				])
+			: factory.createVoidExpression(factory.createIdentifier("undefined"));
 	},
 };
